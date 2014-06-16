7ac79c7fee3f593163489744287d8235077b1d17
CRTX <ruiz.d.christian@gmail.com>
2014-06-16 09:54:05 -0500

    Improved installation script.

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@ git --version 2>&1 >/dev/null
 GIT_IS_AVAILABLE=$?
 
 if [ $GIT_IS_AVAILABLE -eq 0 ]; then
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    echo "Installing..."
 else
     echo "Git is not installed. Please install git."
+    exit 1
 fi
+
+echo "Copying .vimrc to home directory."
+    cp .vimrc ~/.vimrc
+echo "Cloning vundle to home directory."
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Installing plugins..."
+    vim +PluginInstall +qall
+echo Done.
