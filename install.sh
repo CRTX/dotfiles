4f2afb8e3606c5fe1ea6682d1ad3e7c4e317d455
CRTX <ruiz.d.christian@gmail.com>
2014-06-27 11:43:12 -0500

    Improved error logic and status messages.

--- install.sh
+++ install.sh
@@ -4,28 +4,28 @@ VundleUrl=https://github.com/gmarik/Vundle.vim.git
 VundleDirectory=~/.vim/bundle/Vundle.vim
 
 error () {
-    echo "Installation failed. Check for any error messages and try again."
     exit 1;
 }
 
 trap error ERR
 
-echo "Checking to see if you have git installed."
-check=$(dpkg -s "git")
 
-echo "Checking if you have vim-gtk installed."
-check=$(dpkg -s "vim-gtk")
+echo "Checking to see if you have git installed."
+dpkg -s "git" > /dev/null
+
+echo "Checking to see if you have vim-gtk installed."
+dpkg -s "vim-gtk" > /dev/null
 
 echo "Copying .vimrc to home directory."
     cp .vimrc ~/.vimrc
 
-echo "Git cloning vundle to home directory."
+echo "Git cloning vundle plugin to home directory."
 
 if [ ! -d $VundleDirectory ]; then
     git clone $VundleUrl $VundleDirectory
 fi
 
-echo "Installing plugins..."
+echo "Installing vundle plugins..."
     vim +PluginInstall +qall
 
 echo Done.
